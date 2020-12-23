b85932a9b1f814c056c6e18ccc9840c331b9afea
Mees Delzenne <mees.delzenne@gmail.com>
2020-12-23 17:43:09 +0100

    Improved key test

--- chord_test/src/keys.rs
+++ chord_test/src/keys.rs
@@ -14,9 +14,14 @@ pub async fn keys(host: &str, key_amount: usize) -> Result<()> {
 
     let fingers = util::aquire_nodes(host, &cfg).await?;
 
-    let mut node_keys = HashMap::<SocketAddr,usize>::new();
+    let mut node_keys = HashMap::<(SocketAddr,u16),usize>::new();
+    for f in fingers.iter(){
+        node_keys.entry((f.addr,f.addr.port())).or_insert(0);
+    }
     let mut handles = VecDeque::new();
-    for _ in 0..key_amount {
+    eprintln!();
+    for i in 0..key_amount {
+        eprintln!("\rloading key {}",i);
         let finger = rand::thread_rng().gen_range(0..fingers.len());
         let finger = fingers[finger].clone();
         let key = util::random_key(cfg.num_bits);
@@ -26,18 +31,18 @@ pub async fn keys(host: &str, key_amount: usize) -> Result<()> {
         }));
         while handles.len() > 64{
             let succ = handles.pop_front().unwrap().await??;
-            *node_keys.entry(succ.addr).or_insert(0) += 1;
+            *node_keys.entry((succ.addr,succ.addr.port())).or_insert(0) += 1;
         }
     }
 
     for h in handles {
         let succ = h.await??;
-        *node_keys.entry(succ.addr).or_insert(0) += 1;
+        *node_keys.entry((succ.addr,succ.addr.port())).or_insert(0) += 1;
     }
 
     println!("node,number_of_keys");
     for (node,number) in node_keys {
-        println!("{},{}",node,number);
+        println!("{},{}",node.0,number);
     }
 
     Ok(())
