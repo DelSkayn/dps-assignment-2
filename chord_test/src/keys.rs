2854a746349d62c78036212fab0b6873729e4c94
Mees Delzenne <mees.delzenne@gmail.com>
2020-12-23 17:30:58 +0100

    Added code for the final test

--- chord_test/src/keys.rs
+++ chord_test/src/keys.rs
@@ -1,6 +1,10 @@
 use super::util;
 use anyhow::{Context, Result};
 use rand::Rng;
+use std::{
+    collections::{HashMap, VecDeque},
+    net::SocketAddr,
+};
 
 pub async fn keys(host: &str, key_amount: usize) -> Result<()> {
     let host = util::resolve_host(host).await?;
@@ -10,31 +14,30 @@ pub async fn keys(host: &str, key_amount: usize) -> Result<()> {
 
     let fingers = util::aquire_nodes(host, &cfg).await?;
 
-    let mut handles = Vec::new();
+    let mut node_keys = HashMap::<SocketAddr,usize>::new();
+    let mut handles = VecDeque::new();
     for _ in 0..key_amount {
         let finger = rand::thread_rng().gen_range(0..fingers.len());
         let finger = fingers[finger].clone();
-        let max_key = (2 << cfg.num_bits) as u128;
-        let key = rand::thread_rng().gen_range(0..max_key);
-        let key = chord::Key::from_number(key);
-        handles.push(tokio::spawn(async move {
-            chord::rpc::add_key(&finger, key, None).await
+        let key = util::random_key(cfg.num_bits);
+        handles.push_back(tokio::spawn(async move {
+            let successor = chord::rpc::find_successor(&finger,key,None).await?;
+            Result::<chord::Finger>::Ok(successor.unwrap())
         }));
+        while handles.len() > 64{
+            let succ = handles.pop_front().unwrap().await??;
+            *node_keys.entry(succ.addr).or_insert(0) += 1;
+        }
     }
 
     for h in handles {
-        h.await??;
+        let succ = h.await??;
+        *node_keys.entry(succ.addr).or_insert(0) += 1;
     }
 
-    let mut num_keys = Vec::new();
-    for f in fingers {
-        let info = chord::rpc::info(&f, None).await?;
-        num_keys.push((f, info.num_keys));
-    }
-
-    println!("node : keys");
-    for f in num_keys {
-        println!("{}:{}", f.0, f.1)
+    println!("node,number_of_keys");
+    for (node,number) in node_keys {
+        println!("{},{}",node,number);
     }
 
     Ok(())
--- chord_test/src/main.rs
+++ chord_test/src/main.rs
@@ -11,6 +11,7 @@ mod kill_all;
 mod lookup;
 mod simulate;
 mod start;
+mod start_many;
 mod util;
 
 #[derive(Debug, StructOpt)]
@@ -77,6 +78,28 @@ enum Opt {
     Keys { host: String, key_amount: usize },
     /// Stop all the nodes in the network
     KillAll { host: String },
+    /// Start many nodes, possible running multiple nodes on the same cluster node"
+    StartMany{
+        /// The number of nodes to run in this network
+        number_nodes: usize,
+        /// Address for the network nodes to run on.
+        nodes: Vec<String>,
+        /// The number of bits in the key (default 16)
+        #[structopt(short = "b", long = "bits")]
+        num_bits: Option<u8>,
+        /// The number of successors to keep track of (default 4)
+        #[structopt(short = "s", long = "successors")]
+        num_successors: Option<u32>,
+        /// The number of virtual nodes to run on a single node (default 4)
+        #[structopt(long = "nvirtuals")]
+        num_virtual_nodes: Option<u32>,
+        /// The time interval between the various update ticks performed by chord (default 2s)
+        #[structopt(short = "i", long = "interval", parse(try_from_str = parse_duration))]
+        update_interval: Option<Duration>,
+        /// port to run the chord protocol on (default 8080).
+        #[structopt(short = "p", long = "port")]
+        port: Option<u16>,
+    }
 }
 
 #[tokio::main(worker_threads = 1)]
@@ -112,7 +135,7 @@ async fn main() -> Result<()> {
         } => {
             lookup::lookup(&host, repeat, request_per_second, output).await?;
         }
-        Opt::Keys { host, key_amount } => {
+        Opt::Keys { host, key_amount} => {
             keys::keys(&host, key_amount).await?;
         }
         Opt::KillAll { host } => kill_all::kill_all(&host).await?,
@@ -137,6 +160,26 @@ async fn main() -> Result<()> {
                 drop_interval,
                 lookup_interval,
                 test_duration,
+            )
+            .await?;
+        }
+        Opt::StartMany {
+            number_nodes,
+            nodes,
+            num_bits,
+            num_successors,
+            num_virtual_nodes,
+            update_interval,
+            port,
+        } => {
+            start_many::start(
+                nodes,
+                num_bits.unwrap_or(16),
+                num_successors.unwrap_or(4),
+                num_virtual_nodes.unwrap_or(4),
+                update_interval.unwrap_or(Duration::from_secs(2)),
+                port.unwrap_or(8080),
+                number_nodes,
             )
             .await?;
         }
